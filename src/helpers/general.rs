e80dfa1c97bcd261fe62edd14f2f6365c5984748
Jay Zhou <jzilla808@gmail.com>
2023-12-27 00:50:11 -0800

    Add PrintCommand enum and implement print_agent_message method

--- src/helpers/general.rs
+++ src/helpers/general.rs
@@ -1,3 +1,7 @@
+use std::f32::consts::E;
+
+use crate::apis::call_request::call_gpt;
+use crate::helpers::command_line::PrintCommand;
 use crate::models::general::llm::Message;
 
 // Extend AI function to encourage specific output
@@ -17,6 +21,33 @@ pub fn extend_ai_funciton(ai_func: fn(&str) -> &'static str, func_input: &str) -
     Message {
         role: "system".to_string(),
         content: msg,
+    }
+}
+
+// Performs call to LLM GPT
+pub async fn ai_task_request(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> String {
+    // Extend AI function
+    let extended_msg = extend_ai_funciton(function_pass, &msg_context);
+
+    // Print current status
+    PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+    // Get LLM response
+    let llm_response_res: Result<String, Box<dyn std::error::Error + Send>> =
+        call_gpt(vec![extended_msg.clone()]).await;
+
+    // Handle success or try again
+    match llm_response_res {
+        Ok(llm_res) => llm_res,
+
+        Err(_) => call_gpt(vec![extended_msg.clone()])
+            .await
+            .expect("Failed twice to call OpenAI"),
     }
 }
 
